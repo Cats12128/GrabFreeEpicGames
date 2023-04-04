d8e4e4952c9f4e0a6b01e0d5287c2b64fd7b311f
cats12128 <sillybeanz92@gmail.com>
2023-04-04 03:06:43 -0400

    works on grabbing games through browser,  no login

--- GrabFreeEpicGame.py
+++ GrabFreeEpicGame.py
@@ -2,6 +2,12 @@ import pyautogui
 import cv2 as cv
 import numpy as np
 import webbrowser as web
+import win32gui, win32con
+from time import sleep
+import subprocess
+
+
+debug = True
 
 #sets url to open
 url = "https://store.epicgames.com/en-US/"
@@ -9,24 +15,24 @@ url = "https://store.epicgames.com/en-US/"
 #images to match
 freeNowImage = ".\FreeNowImage.png"
 getImage = ".\GetImage.png"
-placeOrderImage= ".\PlaceOrderImage.png"
+placeOrderImage = ".\PlaceOrderImage.png"
+InLibraryImage = ".\InLibraryImage.png"
 currentGame = 0
 
-
-#open {url} in default browser in new tab and raises the window
-#web.open(url, new=0, autoraise=True) ######UNCOMMENT THIS WHEN COMPLETE#####
-
-
-def FindClick(image):
-    #This function will constantly search the page to match the passed image, then click on it.
-    #After click, it will also print out which image it matched for confirmation.
+def FindClick(image): #This function takes an image, constantly search the screen for a match, then clicks it. Then prints which image it matched.
+    
     while True:
+        sleep(2)
         getLoc = pyautogui.locateOnScreen(image, confidence=.95)
+        alreadyInLibrary = pyautogui.locateOnScreen(InLibraryImage, confidence=.95)
         if getLoc:
             x_val = getLoc.left + (getLoc.width/2)
             y_val = getLoc.top + (getLoc.height/2)
             pyautogui.click(x=x_val, y=y_val)
             return print(f'Clicked on location matching {image}')
+        elif alreadyInLibrary:
+            return print('Game already redeemed')
+        
 
 def DrawRectangle(LocAllImg):
     screenshot = pyautogui.screenshot()
@@ -36,41 +42,47 @@ def DrawRectangle(LocAllImg):
     cv.rectangle(screenshot,top_left, bottom_right, color=(0,255,0), thickness=2, lineType=cv.LINE_4)
     cv.imshow("Screenshot", screenshot)
     cv.waitKey(0)
+
+def openEpicInBrowser():#opens url in browser, then maximize
+    #open {url} in default browser in new tab and raises the window
+    web.open(url, new=0, autoraise=True)
+    
+    #assigns the window to hwnd
+    hwnd = win32gui.GetForegroundWindow()
+    #maximizes hwnd (the window)
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+    sleep(2)
+    screenSizeX = pyautogui.size()[0]
+    screenSizeY = pyautogui.size()[1]
+    pyautogui.moveTo(screenSizeX/2, screenSizeY/2)
+
 while True:
+    openEpicInBrowser()
     while True:
-        #looks to match the saved image with one on the screen
-        freeNowLoc = pyautogui.locateAllOnScreen(freeNowImage, confidence=.90)
+        
+        #look for freeNowImage on screen
+        freeNowLoc = pyautogui.locateAllOnScreen(freeNowImage, grayscale=True, confidence=.92)
         #saves result as a list instead of a generator
         freeNowLoc = list(freeNowLoc)
         #if not found it prints NOT FOUND and scrolls down
         if not freeNowLoc:
             print("NOT FOUND")
-            #pyautogui.scroll(-500)
+            pyautogui.scroll(-400)
+                
         elif freeNowLoc:
             print(f'Found image matching {freeNowImage}')
-            print(freeNowLoc)
             if currentGame == 0:
-                numFreeGames = (len(freeNowLoc))
+                numFreeGames = 2
             print(f'Number of Free Games: {numFreeGames}')
+            print(f'Free Now button found at" {freeNowLoc}')
+            x_val = freeNowLoc[currentGame].left + (freeNowLoc[currentGame].width/2)
+            y_val = freeNowLoc[currentGame].top + (freeNowLoc[currentGame].height/2)
+            pyautogui.click(x=x_val, y=y_val)
+            currentGame += 1
             break
-
-    #if found clicks on position found
-    if currentGame >= numFreeGames:
-        break        
-    x_val = freeNowLoc[currentGame].left + (freeNowLoc[currentGame].width/2)
-    y_val = freeNowLoc[currentGame].top + (freeNowLoc[currentGame].height/2)
-    pyautogui.click(x=x_val, y=y_val)
-    currentGame += 1
-
-    '''
-    FindClick(getImage)
+    
+    FindClick(getImage)    
     FindClick(placeOrderImage)
-    '''
-    web.open(url, new=0, autoraise=True)
 
-
-    
-    
-
-
-
+    if currentGame >= numFreeGames:
+        break
