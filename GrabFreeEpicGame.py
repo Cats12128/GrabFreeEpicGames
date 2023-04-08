5eb10fb0c9b327ee7d62f40629b8529922a2f0fe
cats12128 <sillybeanz92@gmail.com>
2023-04-08 02:58:21 -0400

    trying to find best way to click free games TODO: Try selenium module

--- GrabFreeEpicGame.py
+++ GrabFreeEpicGame.py
@@ -22,68 +22,78 @@ INLIBRARYIMAGE = "images\InLibraryImage.png"
 
 def FindClick(image):
     while True:
+        buttonLocation = pyautogui.locateCenterOnScreen(Image1, confidence=.95)
+        if buttonLocation:
+            pyautogui.click(buttonLocation)
+            return print(f'Clicked on location matching {image}')
+ 
+
+def Find1Of2Images(image1, image2):
+    while True:
         sleep(2)
-        getLoc = pyautogui.locateOnScreen(image, confidence=.95)
-        alreadyInLibrary = pyautogui.locateOnScreen(INLIBRARYIMAGE, confidence=.95)
-        if getLoc:
+        getButtonLocation = pyautogui.locateCenterOnScreen(image1, confidence=.95)
+        alreadyInLibrary = pyautogui.locateCenterOnScreen(image2, confidence=.95)
+        if getButtonLocation:
             #x_val = getLoc.left + (getLoc.width/2)
             #y_val = getLoc.top + (getLoc.height/2)
-            pyautogui.click(getLoc.center())
-            return print(f'Clicked on location matching {image}')
+            pyautogui.click(getButtonLocation)
+            return print(f'Clicked on location matching {image1}')
         elif alreadyInLibrary:
             return print('Game already redeemed')
-        
+                
 
 def DrawRectangle(LocAllImg):
     screenshot = pyautogui.screenshot()
     screenshot = np.array(screenshot)
-    top_left = (LocAllImg[0].left, LocAllImg[0].top)
-    bottom_right = (LocAllImg[0].left + LocAllImg[0].width, LocAllImg[0].top + LocAllImg[0].height)
-    cv.rectangle(screenshot,top_left, bottom_right, color=(0,255,0), thickness=2, lineType=cv.LINE_4)
+    for location in range(len(LocAllImg)):
+        top_left = (LocAllImg[location].left, LocAllImg[location].top)
+        bottom_right = (LocAllImg[location].left + LocAllImg[location].width, LocAllImg[location].top + LocAllImg[0].height)
+        cv.rectangle(screenshot,top_left, bottom_right, color=(0,255,0), thickness=2, lineType=cv.LINE_4)
     cv.imshow("Screenshot", screenshot)
     cv.waitKey(0)
 
 def openUrlInBrowserAndMax(url):
     web.open(url, new=0, autoraise=True)
-    sleep(1)
-    chromeWindow = pygetwindow.getWindowsWithTitle("chrome")[0]
-    chromeWindow.maximize()
-
-def openApplication(filepath, windowname):
-    subprocess.call(filepath)
-    sleep(1)
-    window = pygetwindow.getWindowsWithTitle(windowname)[0]
+    sleep(.5)
+    window = pygetwindow.getActiveWindow()
     window.maximize()
 
-def moveCursorToCenter():
+def openApplicationAndMax(filepath, windowname):
+    subprocess.call(filepath)
+    sleep(.5)
+    window = pygetwindow.getActiveWindow()
+    window.maximize()
+
+def moveCursorToCenterScreen():
     screenSizeX = pyautogui.size()[0]
     screenSizeY = pyautogui.size()[1]
     pyautogui.moveTo(screenSizeX/2, screenSizeY/2)
 
-while True:
-    openUrlInBrowserAndMax(url)
+if __name__ == "__main__":
     while True:
-        freeNowLoc = pyautogui.locateAllOnScreen(FREENOWIMAGE, grayscale=True, confidence=.92)
-        freeNowLoc = list(freeNowLoc) #saves result as a list instead of a generator
+        openUrlInBrowserAndMax(url)
+        while True:
+            freeNowButtonLocationS = pyautogui.locateAllOnScreen(FREENOWIMAGE, grayscale=True, confidence=.92)
+            freeNowButtonLocationS = list(freeNowButtonLocationS) #saves result as a list instead of a generator
+            
+            if not freeNowButtonLocationS:
+                print("NOT FOUND")
+                pyautogui.scroll(-400)
+                    
+            elif freeNowButtonLocationS:
+                print(f'Found image matching {FREENOWIMAGE}')
+                if currentGame == 0:
+                    numFreeGames = maxNumOfGames
+                print(f'Number of Free Games: {numFreeGames}')
+                print(f'Free Now button found at" {freeNowButtonLocationS}')
+                x_val = freeNowButtonLocationS[currentGame].left + (freeNowButtonLocationS[currentGame].width/2)
+                y_val = freeNowButtonLocationS[currentGame].top + (freeNowButtonLocationS[currentGame].height/2)
+                pyautogui.click(x=x_val, y=y_val)
+                currentGame += 1
+                break
         
-        if not freeNowLoc:
-            print("NOT FOUND")
-            pyautogui.scroll(-400)
-                
-        elif freeNowLoc:
-            print(f'Found image matching {FREENOWIMAGE}')
-            if currentGame == 0:
-                numFreeGames = maxNumOfGames
-            print(f'Number of Free Games: {numFreeGames}')
-            print(f'Free Now button found at" {freeNowLoc}')
-            x_val = freeNowLoc[currentGame].left + (freeNowLoc[currentGame].width/2)
-            y_val = freeNowLoc[currentGame].top + (freeNowLoc[currentGame].height/2)
-            pyautogui.click(x=x_val, y=y_val)
-            currentGame += 1
-            break
-    
-    FindClick(GETIMAGE)    
-    FindClick(PLACEORDERIMAGE)
+        FindClick(GETIMAGE)    
+        Find1Of2Images(PLACEORDERIMAGE, INLIBRARYIMAGE)
 
-    if currentGame >= numFreeGames:
-        break
+        if currentGame >= numFreeGames:
+            break
--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 import win32gui, win32con
 from time import sleep
 import webbrowser as web
-import pygetwindow as p
+import pygetwindow as window
+import pyautogui as p
 import subprocess
 import cv2 as cv
 import numpy as np
 from GrabFreeEpicGame import DrawRectangle
 
-print("TEST")
-input()
-'''
+
 fni = "images\FreeNowImage.png"
-c=.95           
-locations = p.locateAllOnScreen(fni,confidence=c)
-locations = list(locations)
-DrawRectangle(locations)'''
+c=.95
+sleep(.5)
+while True:
+    print(f"c is {c}")
+    locations = p.locateAllOnScreen(fni, confidence=c)
+    locations = list(locations)
+    c -= .05
+    if locations:
+        print(f"locations found: {locations}")
+        print(f'c is {c}')
+        DrawRectangle(locations)
+        quit()
+    elif c==0:
+        print("c is 0, no matches")
+        quit()
