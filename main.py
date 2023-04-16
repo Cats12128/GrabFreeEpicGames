996db35573edc64e407e83f5bce0c9a84bd5ec85
cats12128 <sillybeanz92@gmail.com>
2023-04-16 02:01:46 -0400

    WORKING Issues: encounters capcha TODO: add to cart instead of get TODO: get 2nd game

--- main.py
+++ main.py
@@ -19,17 +19,27 @@ FREE_NOW_LINK_CLASS = "css-11xvn05" # this is the css for the "Free Now" text
 
 MATURE_CONTINUE_CLASS = "css-1a6we1t" # use By.CLASS_NAME
 GET_BUTTON_CLASS = "css-195czy3"
-PLACE_ORDER_CLASS = "payment-btn payment-order-confirm__btn payment-btn--primary"
+ADD_TO_CART_CLASS = "css-5cj35r" # unused until add to cart is implemented
+PLACE_ORDER_CLASS = "payment-order-confirm"
+FREE_GAME_1_XML_PATH = "/html/body/div[1]/div/div[4]/main/div[2]/div/div/div/span[4]/div/div/section/div/div[1]/div/div/a/div/div/div[1]/div[2]/div/div"
+FREE_GAME_2_XML_PATH = "/html/body/div[1]/div/div[4]/main/div[2]/div/div/div/span[4]/div/div/section/div/div[2]/div/div/a/div/div/div[1]/div[2]/div/div"
 URL = "https://store.epicgames.com/en-US/"
 user = "Mike"
 PATH = "C:\Program Files(86x)\chromedriver.exe"
 
 useProfile = True
 
-def press_button_with_class(html_class, wait_time=5):
+def press_place_order():
+    iframe = driver.find_element(By.CSS_SELECTOR, "#webPurchaseContainer > iframe")
+    print("switching to iframe: {iframe}")
+    driver.switch_to.frame(iframe)
+    press_button_with_custom_By(By.CLASS_NAME, PLACE_ORDER_CLASS)
+    sleep(5)
+
+def press_button_with_custom_By(ByMethod, html_class, wait=5):
     try:
-        button = WebDriverWait(driver, wait_time).until(
-        EC.presence_of_element_located((By.CLASS_NAME, html_class))
+        button = WebDriverWait(driver, wait).until(
+        EC.presence_of_element_located((ByMethod, html_class))
     )
     except:
         print(f'NOT FOUND: {html_class}')
@@ -48,85 +58,50 @@ if useProfile:
     subprocess.call("taskkill /im chrome.exe", shell=True)
     options.add_argument(f"user-data-dir=C:\\Users\\{user}\\AppData\\Local\\Google\\Chrome\\User Data\\")#Path to your chrome profile
     options.add_argument('profile-directory=Default')
-# options.add_argument("start-maximized")
 options.add_argument('--no-sandbox')
+# options.add_argument("start-maximized")
 # options.page_load_strategy = 'eager'
+# options.add_argument("--headless=new")
 service = ChromeService(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(executable_path=PATH, options=options, service=service)
-
-print("Opening Chrome")
+driver = webdriver.Chrome(options=options, service=service)
+print("Opening Chrome...")
 driver.get(URL)
-print(f'Opening Chrome, url is: {driver.current_url}')
+print(f'URL is: {driver.current_url}')
 
-# Now set the cookie. This one's valid for the entire domain
-# cookie = {'name' : 'foo', 'value' : 'bar'}
-# driver.add_cookie(cookie)
-# # And now output all the available cookies for the current URL
-# driver.get_cookies()
-# print(f'cookie is: {cookie}')
+press_button_with_custom_By(By.CLASS_NAME, FREE_NOW_LINK_CLASS)
+print(f'\nurl is: {driver.current_url}')
+print("\nright before press button(MATURE CONTINUE)")
+press_button_with_custom_By(By.CLASS_NAME, MATURE_CONTINUE_CLASS)
+print("\nright before press button(GET BUTTON)")
+press_button_with_custom_By(By.CLASS_NAME, GET_BUTTON_CLASS)
+sleep(3)
+press_place_order()
 
-press_button_with_class(FREE_NOW_LINK_CLASS)
-#FREE_NOW_ELEMENT = driver.find_elements(By.CLASS_NAME, FREE_NOW_LINK_CLASS)
+# driver.switch_to.frame(driver.find_elements(By.TAG_NAME, "iframe")[0])
+# ele = driver.find_elements(By.XPATH, '//*[@id]')
+# for x in ele:
+#     print(x.tag_name, x.get_attribute('id'))
 
-# if FREE_NOW_ELEMENT:
-#     print("ELEMENT FOUND: ")
-#     for e in FREE_NOW_ELEMENT:
-#         print(e)
-# else:
-#     print("ELEMENT NOT FOUND")
 
-# FREE_NOW_ELEMENT[0].click()
-print(f'url is: {driver.current_url}')
-print("right before press button(MATURE CONTINUE)")
-press_button_with_class(MATURE_CONTINUE_CLASS)
-print("right before press button(GET BUTTON)")
-press_button_with_class(GET_BUTTON_CLASS)
-sleep(5) 
+# Store iframe web element
 
 
 
-driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
 
-element = driver.find_element(By.CLASS_NAME, "payment-order-confirm")
-if element:
-    element.click()
-    print("we done it")
+
+# print("NEXT IFRAME")
+# ele = driver.find_elements(By.TAG_NAME, "fpt_frame")
+# for x in ele:
+#     print(x.tag_name, x.get_attribute('id'))
+#     driver.switch_to.frame(x)
+
+
+
+# driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+
+# element = driver.find_element(By.CLASS_NAME, "payment-order-confirm")
+# if element:
+#     element.click()
+#     print(f"we clicked: {element}")
 
 # press_button_with_class(PLACE_ORDER_CLASS)
-
-    
-
-    
-
-# try:
-#     input("WAITING")
-#     button = WebDriverWait(driver, 5).until(
-#     EC.presence_of_element_located((By.CSS_SELECTOR, "button.payment-btn payment-order-confirm__btn payment-btn--primary"))
-# )
-    
-# except:
-#     print(f'NOT FOUND: Place Order')
-#     button = False
-    
-# finally:
-#     if button:
-#         print(f'Clicking Place Order')
-#         button.click()
-#     pass
-
-
-# MATURE_CONTINUE_BUTTON = driver.find_element(By.CLASS_NAME, MATURE_CONTINUE_CLASS)
-
-
-
-# GET_BUTTON = driver.find_element(By.CLASS_NAME, GET_BUTTON_CLASS)
-
- 
-
-
-
-
-
-
-
-
